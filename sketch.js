3ae535bb9ff3b8de04292f634ae48746e8adb56b
SilviaWebDesign <silvia.lamastra@mail.polimi.it>
2025-10-14 15:56:25 +0200

    setup

--- sketch.js
+++ sketch.js
@@ -1,15 +1,16 @@
+let table;
+
 function preload() {
   // put preload code here
+  table = loadTable("dataset.csv", "csv", "header");
+
 }
 
 function setup() {
   createCanvas(windowWidth, windowHeight);
   // put setup code here
-  const message =
-    "This is a template repository\nfor the course Laboratorio di Computergrafica\nCommunication Design, Politecnico di Milano";
-  textAlign(CENTER, CENTER);
-  textSize(16);
-  text(message, width / 2, height / 2);
+// test 
+console.log (table);
 }
 
 function draw() {

28c7eac0021a4f9f7faeb218ccf528825503f677
SilviaWebDesign <silvia.lamastra@mail.polimi.it>
2025-10-14 17:00:29 +0200

    carico dati

--- sketch.js
+++ sketch.js
@@ -7,10 +7,32 @@ function preload() {
 }
 
 function setup() {
-  createCanvas(windowWidth, windowHeight);
   // put setup code here
+  let outerPadding = 20;
+  let innerPadding = 10;
+  let itemSize = 30;
+
+  // calcolo il numero di colonne 
+  let cols =  floor((windowWidth - outerPadding * 2) / (itemSize + innerPadding));
+  let rows = table.getRowCount() / cols;
+
+  let totalHeight = outerPadding * 2 + rows * itemSize + (rows - 1) * innerPadding;
+
+  // creo il canvas 
+  createCanvas(windowWidth, windowHeight);
+  background(0, 150, 150);
+
+  for (let rowNumber = 0; rowNumber < table.getRowCount(); rowNumber ++) {
+     
+    // carico dati della riga 
+    let data = table.getRow(rowNumber).obj;
+    console.log(data);
+  }
 // test 
-console.log (table);
+console.log ("tabella:", table);
+console.log ("colonne:", cols);
+console.log ("cols:", cols, "rows:", rows);
+
 }
 
 function draw() {

7a8bbb57c1f545c1331086352c30597e3c7f765c
SilviaWebDesign <silvia.lamastra@mail.polimi.it>
2025-10-20 23:28:58 +0200

    aggiornamento lavoro

--- sketch.js
+++ sketch.js
@@ -4,18 +4,13 @@ function calcolaNumeroElementiPerRiga(windowWidth, outerPadding, glifoSize, inne
   return floor((windowWidth - outerPadding * 2) / (glifoSize + innerPadding));
 }
 
-//function trovaDimensioneCerchio(data, glifoSize) {
-  // definisco  IN BASE AI DATI DELLA COLONNA 0 /////////
-    // prendo size value dalla colonna 0
-  //  let dimesioneCerchioIniziale = data["column0"];
-
-    // ottieni minimo e massimo da tutti i dati di colonna 0
-  //  let dimensioniTotaliCerchio = table.getColumn("column0");
-  //  let dimensioniMinimeCerchio = min(dimensioniTotaliCerchio);
-  //  let dimensioniMassimeCerchio = max(dimensioniTotaliCerchio);
-  //  let dimensioneCerchioFinale = map (dimesioneCerchioIniziale, dimensioniMinimeCerchio, dimensioniMassimeCerchio, 1, glifoSize);
-  ////////////////////////////////////////////////////////////////////////////////////
-//}
+function calcolaDimensioneCerchioFinale(data, glifoSize) {
+  const dimesioneCerchioIniziale = data["column0"];
+  const dimensioniTotaliCerchio = table.getColumn("column0");
+  const dimensioniMinimeCerchio = min(dimensioniTotaliCerchio);
+  const dimensioniMassimeCerchio = max(dimensioniTotaliCerchio);
+  return map(dimesioneCerchioIniziale, dimensioniMinimeCerchio, dimensioniMassimeCerchio, 1, glifoSize);
+}
 
 
 
@@ -25,24 +20,16 @@ function preload() {
 
 function setup() {
 
-  // CONFIGURAZIONE ///////////////
-  // margine esterno del canvas.
-  let outerPadding = 20;
-  // spazio tra i glifi
-  let innerPadding = 10;
-  // dimensione(massima) lato del glifo
-  let glifoSize = 35;
-  let maxAngle= 360;
-  //////////////////////////////////
+  const margineEsterno = 20;
+  const spazioTraGlifi = 10;
+  const dimensioneMassimaGlifo = 35;
+  const ampiezzaMassimaAngolo= 360;
 
-  // calcolo il numero di colonne in base a quante celle 
-  // entrano in una riga in funzione della larghezza dello schermo
-  const numeroElementiPerRiga = calcolaNumeroElementiPerRiga(windowWidth, outerPadding, glifoSize, innerPadding)
-  let numeroRigheNecessarie = table.getRowCount() / numeroElementiPerRiga;
+  // calcolo il numero di colonne in base a quante celle entrano in una riga in funzione della larghezza dello schermo
+  const numeroElementiPerRiga = calcolaNumeroElementiPerRiga(windowWidth, margineEsterno, dimensioneMassimaGlifo, spazioTraGlifi)
+  const numeroRigheNecessarie = table.getRowCount() / numeroElementiPerRiga;
 
-// let totalHeight = outerPadding * 2 + rows * glifoSize + (rows - 1) * innerPadding;
-
-// creo la canvas 
+  // creo la canvas 
   createCanvas(windowWidth, windowHeight);
   background(15);
 
@@ -51,30 +38,19 @@ function setup() {
   let numeroRiga = 0;
 
   // Loop su tutte le righe del CSV
-  for (let rowNumber = 0; rowNumber < table.getRowCount(); rowNumber ++) {
+  for (let i = 0; i < table.getRowCount(); i ++) {
     
     // carico dati della riga 
-    let data = table.getRow(rowNumber).obj;
-
-//const dimensioneCerchioFinale= trovaDimensioneCerchio(data, glifoSize);
-// definisco  IN BASE AI DATI DELLA COLONNA 0 /////////
-    // prendo size value dalla colonna 0
-    let dimesioneCerchioIniziale = data["column0"];
-
-    // ottieni minimo e massimo da tutti i dati di colonna 0
-    let dimensioniTotaliCerchio = table.getColumn("column0");
-    let dimensioniMinimeCerchio = min(dimensioniTotaliCerchio);
-    let dimensioniMassimeCerchio = max(dimensioniTotaliCerchio);
-    let dimensioneCerchioFinale = map (dimesioneCerchioIniziale, dimensioniMinimeCerchio, dimensioniMassimeCerchio, 1, glifoSize);
-////////////////////////////////////////////////////////////////////////////////////
+    const data = table.getRow(i).obj;
+    const dimensioneCerchioFinale = calcolaDimensioneCerchioFinale(data, dimensioneMassimaGlifo);
 
 // DEFNISCO I COLORI IN BASE AI VALORI DELLA COLONNA 2 //////////////////////////
     // variabile per il colore 
-    let colorValue = data["column2"];
-    let allColorValues = table.getColumn("column2");
-    let minColorValue = min(allColorValues);
-    let maxColorValue = max(allColorValues);
-    let colorValueMapped = map(colorValue, minColorValue, maxColorValue, 0, 1);
+    const indicatoreColore = data["column2"];
+    const listaCompletaColori = table.getColumn("column2");
+    const indicatoreMinimoColore = min(listaCompletaColori);
+    const indicatoreMassimoColore = max(listaCompletaColori);
+    const valoreColoreMappato = map(indicatoreColore, indicatoreMinimoColore, indicatoreMassimoColore, 0, 1);
 /////////////////////////////////////////////////////////////////////////////////////
 
     // DEFINISCO QUALI COLORI VOGLIO MAPPARE
@@ -82,15 +58,15 @@ function setup() {
     let color2 = color(150, 0, 150, 100);
     
     // MAPPO IL RANGE DI SFUMATURE TRA COLOR1 E COLOR2
-    let mappedColor = lerpColor (color1, color2, colorValueMapped);
+    let mappedColor = lerpColor (color1, color2, valoreColoreMappato);
 
     // DISEGNO CERCHIO CON SCALED VALUE///////////////////////////
     fill(mappedColor);
     noStroke();
     rectMode(CENTER);
     // calcola posizione cella
-    let xPos = outerPadding + numeroColonna * (glifoSize + innerPadding);
-    let yPos = outerPadding + numeroRiga * (glifoSize + innerPadding);
+    let xPos = margineEsterno + numeroColonna * (dimensioneMassimaGlifo + spazioTraGlifi);
+    let yPos = margineEsterno + numeroRiga * (dimensioneMassimaGlifo + spazioTraGlifi);
     circle(xPos, yPos, dimensioneCerchioFinale);
     ////////////////////////////////////////////////////////////////////
 
@@ -102,13 +78,13 @@ function setup() {
     let allQuadValues = table.getColumn("column3");
     let minQuadValue = min(allQuadValues);
     let maxQuadValue = max(allQuadValues);
-    let quadSize = map (quadValue, minQuadValue, maxQuadValue, 1, glifoSize);
+    let quadSize = map (quadValue, minQuadValue, maxQuadValue, 1, dimensioneMassimaGlifo);
 ////////////////////////////////////////////////////////////////////////////////////
 
     // CREO UN QUADRATO CON QUADSIZE//////////
     color1 = color(0, 100, 0, 100);
     color2 = color(255, 255, 0, 150);
-    mappedColor = lerpColor (color1, color2, colorValueMapped);
+    mappedColor = lerpColor (color1, color2, valoreColoreMappato);
     fill(mappedColor);
     square(xPos, yPos, quadSize);
     ////////////////////////////////////////////////////////
@@ -121,26 +97,26 @@ function setup() {
     let allRotationValues = table.getColumn("column1");
     let minRotationValue = min(allRotationValues);
     let maxRotationValue = max(allRotationValues);
-    let angle = map (rotationValue, minRotationValue, maxRotationValue, 1, maxAngle);
+    let angle = map (rotationValue, minRotationValue, maxRotationValue, 1, ampiezzaMassimaAngolo);
     ////////////////////////////////////////////////////////////////////////////////////
 
     //CREO UN ARC TRAMITE ANGLE////////////////////////////////////////
     angleMode(DEGREES);
     color1 = color(0, 150, 100, 200);
     color2 = color(150, 0, 200, 150);
-    mappedColor = lerpColor (color1, color2, colorValueMapped);
+    mappedColor = lerpColor (color1, color2, valoreColoreMappato);
     fill(mappedColor);
-    arc(xPos, yPos, glifoSize, glifoSize, angle, PI + QUARTER_PI)
+    arc(xPos, yPos, dimensioneMassimaGlifo, dimensioneMassimaGlifo, angle, PI + QUARTER_PI)
     /////////////////////////////////////////////////////////////////
 
-// aumento colCount
-numeroColonna++;
+    // aumento colCount
+    numeroColonna++;
 
-// controllo se siamo a fine riga 
-if(numeroColonna == numeroElementiPerRiga) {
-  numeroColonna = 0;
-  numeroRiga++;
-}
+    // controllo se siamo a fine riga 
+    if(numeroColonna == numeroElementiPerRiga) {
+      numeroColonna = 0;
+      numeroRiga++;
+    }
   }
 // test 
 console.log ("tabella:", table);
